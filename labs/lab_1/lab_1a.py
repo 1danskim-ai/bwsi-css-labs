f30191944671a9b5e65660f3d30f69e63fcbd711
1danskim-ai <1danskim@gmail.com>
2026-02-15 17:40:56 -0500

    My Introdcution!

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -9,8 +9,10 @@ def main():
     print("Hello World!")
 
     name = "Daniel Kim" # TODO: Insert your name between the double quotes
+    intro = "Hello all! My name is Daniel Kim, and I am a junior at Southern Lehigh High School (near Bethlehem, Pennsylvania). I am on my schools FRC team, mainly as a programmer, with a passion for physics and calculus. My visit to the Kennedy Space Center when I was around 10 made me really interested in all things space and engineering. This course seems like an exciting step in translating my interest and coursework into something tangible, beyond competition robotics."
 
     print(f"{name}, Welcome to the CSS course!")
+    print("My introduction: " + intro)
 
 if __name__ == "__main__":
     main()
